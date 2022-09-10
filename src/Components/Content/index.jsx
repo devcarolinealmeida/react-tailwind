0ad5311c6c9f23d9b1d70acdc50f3265e45d0a98
devcarolinealmeida <carolineitalia85@gmail.com>
2022-09-10 12:35:26 +0200

    style: ajusting width

--- src/Components/Content/index.jsx
+++ src/Components/Content/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Content = () => {
     return ( 
-        <div className='text-center'>
+        <div className='text-center w-full'>
             <h1 className='text-4xl text-secondary mb-2'>Dev Newsletter</h1>
             <p className='text-xl text-gray-700 mb-2'>A newsletter of Development articles, news and cool projects</p>
         </div>
--- src/Components/Form/index.jsx
+++ src/Components/Form/index.jsx
@@ -3,7 +3,7 @@ import Button from '../Button';
 
 const Form = () => {
     return ( 
-        <div>
+        <div className='w-full'>
             <label className='text-sm font-bold text-gray-700 mb-2'>Email:</label>
             <input className='text-sm w-full mb-4 pl-2 py-2 rounded border shadow focus:outline-none hover:border-hover' type="email" placeholder='Enter your email' />
             <Button />
