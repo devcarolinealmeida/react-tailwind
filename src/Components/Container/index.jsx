0215dc1e79417feaf19472d2724e72313b4541cb
devcarolinealmeida <carolineitalia85@gmail.com>
2022-09-12 11:00:53 +0200

    refactor Icon Bell and style effect initial

--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import Content from '../Content/index'
 import Form from '../Form/index'
 
-import { BsFillBellFill } from 'react-icons/bs'
+import Bell from '../Bell';
+import EffectBell from '../Bell/EffectBell';
 
 
 const Container = () => {
     return ( 
         <section className='bg-white font-roboto p-10 flex flex-col items-center justify-center gap-6 rounded-lg lg:w-1/2 lg:rounded-l-none'>
-            <div>
-                <BsFillBellFill size={70}/>
+            <div className='relative group'>
+                <EffectBell />
+                <Bell />
             </div>
             <Content />
             <Form />
